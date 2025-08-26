480fcf6a722b8de4a854f4bbf256eb0f446f50e3
xMohnad <mohnad00967@gamil.com>
2025-08-26 16:28:17 +0300

    refactor: Split main screen into OpenScreen and MkvManagScreen

--- src/pyinkr/main.py
+++ src/pyinkr/main.py
@@ -2,19 +2,63 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from textual import work
 from textual.app import App, ComposeResult
 from textual.binding import Binding
+from textual.screen import Screen
 from textual.widgets import Footer, Header, TabbedContent, TabPane
 from textual_fspicker import FileOpen, FileSave
 
 from pyinkr.mkv_manager import MkvManager
 from pyinkr.widgets import InfoTree, ListTrack
 
+if TYPE_CHECKING:
+    from pyinkr.main import Inkr
 
-class Inkr(App):
-    CSS_PATH = "style.tcss"
-    BINDINGS = [Binding("o", "open", "Open")]
+
+class OpenScreen(Screen[MkvManager]):
+    BINDINGS = [
+        Binding("o", "open", "Open"),
+        Binding("escape", "back", "Back", tooltip="Back To Opened MKV"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+
+    def action_open(self):
+        def open(path):
+            if path:
+                self.loading = True
+                self.__init_manager(path)
+
+        self.app.push_screen(FileOpen(), open)
+
+    @work(exclusive=True, thread=True, group="init_manager")
+    async def __init_manager(self, path):
+        try:
+            manager = MkvManager(path)
+            self.dismiss(manager)
+        except Exception as e:
+            self.notify(str(e), severity="error")
+
+        self.loading = False
+
+    async def action_back(self):
+        if hasattr(self.app, "manager"):
+            await self.run_action("app.back")
+        else:
+            self.notify("Open MKV First", severity="warning")
+
+
+class MkvManagScreen(Screen):
+    BINDINGS = [
+        Binding("s", "save", "Save"),
+        Binding("escape", "back_to_open", "Back To Open Screen", False),
+    ]
+    app: Inkr
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -26,35 +70,30 @@ class Inkr(App):
                 yield ListTrack(id="track")
         yield Footer()
 
-    async def action_open(self):
-        self.query_one(TabbedContent).loading = True
-        self.push_screen(FileOpen(), callback=self.initialize_mkv_manager)  # pyright: ignore
-
-    @work(exclusive=True, thread=True)
-    async def initialize_mkv_manager(self, file):
-        if not file:
-            return
-
-        try:
-            self.mkv_manager = MkvManager(file)
-        except Exception as e:
-            self.query_one(TabbedContent).loading = False
-            self.notify(str(e), severity="error")
-            return self.log(e)
-
-        self.query_one(InfoTree).data = self.mkv_manager.info_json or {}
-        self.query_one(ListTrack).on_mount()
-        self.bind("s", "save", description="Save")
-        self.query_one(TabbedContent).loading = False
+    @work(exclusive=True)
+    async def action_back_to_open(self):
+        self.app.manager = await self.app.push_screen_wait("Open")
+        await self.recompose()
 
     @work(exclusive=True)
     async def action_save(self):
-        save_path = await self.push_screen(
-            FileSave(default_file=self.mkv_manager.filepath.name),
-            wait_for_dismiss=True,
+        save_path = await self.app.push_screen_wait(
+            FileSave(default_file=self.app.manager.filepath.name)
         )
         if save_path:
-            self.mkv_manager.save(save_path)
+            self.app.manager.save(save_path)
+
+
+class Inkr(App):
+    manager: "MkvManager"
+
+    CSS_PATH = "style.tcss"
+    SCREENS = {"Open": OpenScreen, "MkvManager": MkvManagScreen}
+
+    @work(exclusive=True)
+    async def on_mount(self):
+        self.manager = await self.push_screen_wait("Open")
+        self.push_screen("MkvManager")
 
 
 def main():
